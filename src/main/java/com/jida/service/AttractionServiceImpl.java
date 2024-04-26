1dc1632ddb6b7e7d49341bbba0956542bd5a2bce
dain0826 <62243967+dain0826@users.noreply.github.com>
2024-04-26 23:19:06 +0900

    [FEAT] 여행지 service 코드 작성 (#9)

--- src/main/java/com/jida/service/AttractionServiceImpl.java
+++ src/main/java/com/jida/service/AttractionServiceImpl.java
@@ -1,5 +1,31 @@
 package com.jida.service;
 
-public class AttractionServiceImpl implements AttractionService {
+import static com.fasterxml.jackson.databind.type.LogicalType.Map;
+import static com.jida.dto.res.attraction.AttractionListResponseDto.*;
 
+import com.jida.dto.res.attraction.AttractionListResponseDto;
+import com.jida.mapper.AttractionMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AttractionServiceImpl implements AttractionService {
+    private final AttractionMapper attractionMapper;
+    @Override
+    public AttractionListResponseDto showList(Map<String, Object> map) {
+        List<AttractionList> list = new ArrayList<>();
+        list = attractionMapper.showList(map).stream()
+                .map(AttractionList::of)
+                .collect(Collectors.toList());
+        return AttractionListResponseDto.of(list);
+
+    }
 }
