15f218ce68823636393bd199649266f57a930146
mmalfoy <stpmalfoy@gmail.com>
2024-05-10 17:53:27 +0900

    [FIX] http 메서드 수정 (#25)

--- src/main/java/com/jida/controller/MemberController.java
+++ src/main/java/com/jida/controller/MemberController.java
@@ -1,11 +1,7 @@
 package com.jida.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.jida.dto.req.MemberRequestDto;
 import com.jida.dto.req.MemberSaveRequestDto;
@@ -20,6 +16,7 @@ import static com.jida.constants.SuccessCode.*;
 
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
 @RequestMapping("/api/member")
 public class MemberController {
 	
@@ -31,7 +28,7 @@ public class MemberController {
 		return MemberResponse.newResponse(SIGNUP_SUCCESS);
 	}
 	
-	@GetMapping()
+	@PostMapping("/login")
 	public ResponseEntity<MemberDetailResponse> login(@RequestBody MemberRequestDto memberRequestDto){
 		MemberDetailResponseDto responseDto = memberService.loginMember(memberRequestDto);
 		return MemberDetailResponse.newResponse(LOGIN_SUCCESS, responseDto);
