5fd4fd0a87c956128af062ea345c8249ff65e7e6
박지훈 <pjh2996@naver.com>
2024-05-10 15:28:37 +0900

    [FIX] Member & Board 객체 추가 #19

--- src/main/java/com/jida/domain/Post.java
+++ src/main/java/com/jida/domain/Post.java
@@ -1,5 +1,10 @@
 package com.jida.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Post {
 	private long postId;
 	private String title;
@@ -8,50 +13,9 @@ public class Post {
 	private long hit;
 	private long boardId;
 	private long memberId;
-	
-	public long getPostId() {
-		return postId;
-	}
-	public void setPostId(long postId) {
-		this.postId = postId;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCreatedAt() {
-		return createdAt;
-	}
-	public void setCreatedAt(String createdAt) {
-		this.createdAt = createdAt;
-	}
-	public long getHit() {
-		return hit;
-	}
-	public void setHit(long hit) {
-		this.hit = hit;
-	}
-	public long getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(long boardId) {
-		this.boardId = boardId;
-	}
-	public long getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(long memberId) {
-		this.memberId = memberId;
-	}
-	
+	private Member member;
+	private Board board;
+
 	public static Post creatPost(long memberId, long boardId, String title, String content) {
 		Post post = new Post();
 		post.memberId = memberId;
