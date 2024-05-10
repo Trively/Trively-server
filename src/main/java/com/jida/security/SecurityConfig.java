63765c188389ce66aba25398ec0655ebfd57106b
jihun <pjh2996@naver.com>
2024-05-10 20:26:07 +0900

    [FEAT] 스프링 시큐리티 CORS 에러 처리 추가 #27

---
+++ src/main/java/com/jida/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.jida.security;
+
+import static org.springframework.http.HttpMethod.POST;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(apiConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> {
+                            authorize.requestMatchers(POST,"/api/member/login").permitAll();
+                            authorize.requestMatchers(POST, "/api/member").permitAll();
+//                            authorize.requestMatchers("/api/**").hasRole(USER.name());
+                        }
+                )
+                .build();
+    }
+
+    private CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}
