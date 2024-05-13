a98f4286a7ca9773728d90b04aec31cff72110ea
박지훈 <pjh2996@naver.com>
2024-05-13 16:46:40 +0900

    [REFACTOR] 도메인 setter 삭제 (#32)

--- src/main/java/com/jida/domain/Board.java
+++ src/main/java/com/jida/domain/Board.java
@@ -1,19 +1,9 @@
 package com.jida.domain;
 
+import lombok.Getter;
+
+@Getter
 public class Board {
 	private long boardId;
 	private String boardName;
-	public long getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(int boardId) {
-		this.boardId = boardId;
-	}
-	public String getBoardName() {
-		return boardName;
-	}
-	public void setBoardName(String boardName) {
-		this.boardName = boardName;
-	}
-	
 }
